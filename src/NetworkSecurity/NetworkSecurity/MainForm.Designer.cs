09747b6b3ef7b904c8cb73e21348e2248ba6a72f
elhamest <e.estedlali@gmail.com>
2018-04-29 20:42:55 +0430

    summering some codes

--- src/NetworkSecurity/NetworkSecurity/MainForm.Designer.cs
+++ src/NetworkSecurity/NetworkSecurity/MainForm.Designer.cs
@@ -321,8 +321,8 @@ namespace NetworkSecurity
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.ClientSize = new System.Drawing.Size(1014, 623);
             this.Controls.Add(this.progress);
             this.Controls.Add(this.gbCrypto);
--- src/NetworkSecurity/NetworkSecurity/MainForm.cs
+++ src/NetworkSecurity/NetworkSecurity/MainForm.cs
@@ -9,19 +9,17 @@ namespace NetworkSecurity
 {
     public partial class MainForm : Form
     {
-        public string Key => txtKey.Text ?? "default key";
-        
-        public EnumCryptographyAlgorithms SelectedCryptographyAlgorithm  //eli
+        public string GetKey()
         {
-            get
-            {
-                Enum.TryParse(cmbCryptoTypes.SelectedItem.ToString(), true, out EnumCryptographyAlgorithms algorithm);
-                return algorithm;
-            }
+            return string.IsNullOrWhiteSpace(txtKey.Text) ? "default_key" : txtKey.Text;
+        }
+
+        public EnumCryptographyAlgorithms GetSelectedAlgorithm()
+        {
+            return (EnumCryptographyAlgorithms)cmbCryptoTypes.SelectedItem;            
         }
 
 
-        //eli
         public FileInfo EncryptedFileInfo => new FileInfo(lblBrowseEncryptedBinaryFile.Text);
         public FileInfo DecryptedFileInfo => new FileInfo(lblBrowseDecryptedBinaryFile.Text);
 
@@ -34,9 +32,7 @@ namespace NetworkSecurity
             //
             txtDecryptedText.TextChanged += (sender, args) => rbtnSelectTextType.Checked = true;
             txtEncryptedText.TextChanged += (sender, args) => rbtnSelectTextType.Checked = true;
-            lblBrowseDecryptedBinaryFile.TextChanged += (sender, args) => rbtnSelectBinaryType.Checked = true;
-            lblBrowseEncryptedBinaryFile.TextChanged += (sender, args) => rbtnSelectBinaryType.Checked = true;
-
+            
             btnBrowseDecryptedBinaryFile.Click += BtnBrowseBinaryFile_Click;
             btnBrowseEncryptedBinaryFile.Click += BtnBrowseBinaryFile_Click;
 
@@ -51,7 +47,7 @@ namespace NetworkSecurity
             {
                 CheckFileExists = true,
                 Multiselect = false,
-                Filter = @"Binary files (*.exe;*.dll;*.bat)|*.exe;*.dll;*.bat|All files (*.*)|*.*",
+                Filter = @"Binary files (*.exe;*.dll;*.bat;*.encrypted)|*.exe;*.dll;*.bat;*.encrypted|All files (*.*)|*.*",
                 Title = @"Open binary file"
             };
 
@@ -61,15 +57,17 @@ namespace NetworkSecurity
                     lblBrowseDecryptedBinaryFile.Text = ofd.FileName;
                 else
                     lblBrowseEncryptedBinaryFile.Text = ofd.FileName;
+
+                rbtnSelectBinaryType.Checked = true;
             }
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            splitContainer.SplitterDistance = splitContainer.Size.Width / 2 - 2;   //eli
+            // fill algorithm combo box on main form
             foreach (EnumCryptographyAlgorithms al in Enum.GetValues(typeof(EnumCryptographyAlgorithms)))
             {
-                cmbCryptoTypes.Items.Add(al.ToString());   //eli
+                cmbCryptoTypes.Items.Add(al);   
             }
 
             cmbCryptoTypes.SelectedIndex = 0;
@@ -84,32 +82,32 @@ namespace NetworkSecurity
             {
                 if (rbtnSelectTextType.Checked) // Text Encryption
                 {
-                    txtEncryptedText.Text = txtDecryptedText.Text.Encrypt(Key, SelectedCryptographyAlgorithm);
+                    txtEncryptedText.Text = txtDecryptedText.Text.Encrypt(GetKey(), GetSelectedAlgorithm());
                     timer.Stop();
                 }
-                else
+                else // is binary 
                 {
                     var deFile = DecryptedFileInfo;
-                    if (deFile.Exists && !string.IsNullOrEmpty(deFile.DirectoryName)) // Binary Encryption
+                    if (deFile.Exists) // Binary Encryption
                     {
                         var decryptedBytes = File.ReadAllBytes(deFile.FullName);
-                        var encryptedBytes = decryptedBytes.Encrypt(Key, SelectedCryptographyAlgorithm);
-                        var encryptedFile = Path.Combine(deFile.DirectoryName, deFile.Name + ".encrypted");
-                        File.WriteAllBytes(encryptedFile, encryptedBytes);
-                        lblBrowseEncryptedBinaryFile.Text = encryptedFile;
+                        var encryptedBytes = decryptedBytes.Encrypt(GetKey(), GetSelectedAlgorithm());
+                        var encryptedFilePath = Path.Combine(deFile.DirectoryName, deFile.Name + ".encrypted");
+                        File.WriteAllBytes(encryptedFilePath, encryptedBytes);
+                        lblBrowseEncryptedBinaryFile.Text = encryptedFilePath;
                         timer.Stop();
-                        MessageBox.Show($@"The encrypted file stored in: {encryptedFile}");
+                        MessageBox.Show($@"The encrypted file stored in: {encryptedFilePath}");
                     }
                 }
             }
             catch (Exception exp)
             {
                 timer.Stop();
-                MessageBox.Show(exp.Message, @"Encryption Error", MessageBoxButtons.OK, MessageBoxIcon.Error);  //eli
+                MessageBox.Show(exp.Message, @"Encryption Error", MessageBoxButtons.OK, MessageBoxIcon.Error); 
             }
             finally
             {
-                lblEncryptDuration.Text = $@"Encrypt Duration: {timer.ElapsedMilliseconds}ms"; //eli
+                lblEncryptDuration.Text = $@"Encrypt Duration: {timer.ElapsedMilliseconds}ms"; 
             }
 
         }
@@ -117,7 +115,7 @@ namespace NetworkSecurity
         private void BtnDecrypt_Click(object sender, EventArgs e)
         {
             var timer = Stopwatch.StartNew();
-            var decryptionAlgorithm = SelectedCryptographyAlgorithm;
+            var decryptionAlgorithm = GetSelectedAlgorithm();
             if (decryptionAlgorithm.GetCryptoServiceProvider() is HashAlgorithm)
             {
                 MessageBox.Show(@"Can not decrypt hashed contents!!!", @"Decryption Warning", MessageBoxButtons.OK,
@@ -128,21 +126,21 @@ namespace NetworkSecurity
             {
                 if (rbtnSelectTextType.Checked) // Text Decryption
                 {
-                    txtDecryptedText.Text = txtEncryptedText.Text.Decrypt(Key, SelectedCryptographyAlgorithm);
+                    txtDecryptedText.Text = txtEncryptedText.Text.Decrypt(GetKey(), GetSelectedAlgorithm());
                     timer.Stop();
                 }
                 else
                 {
-                    var enFile = EncryptedFileInfo;
-                    if (enFile.Exists && !string.IsNullOrEmpty(enFile.DirectoryName)) // Binary Decryption   //eli:DirectoryName
+                    var encFile = EncryptedFileInfo;
+                    if (encFile.Exists) // Binary Decryption
                     {
-                        var encryptedBytes = File.ReadAllBytes(enFile.FullName);
-                        var decryptedBytes = encryptedBytes.Decrypt(Key, SelectedCryptographyAlgorithm);
-                        var decryptedFile = Path.Combine(enFile.DirectoryName, enFile.Name.Replace(".encrypted", ""));
-                        File.WriteAllBytes(decryptedFile, decryptedBytes);
-                        lblBrowseDecryptedBinaryFile.Text = decryptedFile;
+                        var encryptedBytes = File.ReadAllBytes(encFile.FullName);
+                        var decryptedBytes = encryptedBytes.Decrypt(GetKey(), GetSelectedAlgorithm());
+                        var decryptedFilePath = Path.Combine(encFile.DirectoryName, encFile.Name.Replace(".encrypted", ""));
+                        File.WriteAllBytes(decryptedFilePath, decryptedBytes);
+                        lblBrowseDecryptedBinaryFile.Text = decryptedFilePath;
                         timer.Stop();
-                        MessageBox.Show($@"The decrypted file stored in: {decryptedFile}");
+                        MessageBox.Show($@"The decrypted file stored in: {decryptedFilePath}");
                     }
                 }
             }
@@ -156,6 +154,7 @@ namespace NetworkSecurity
                 lblDecryptDuration.Text = $@"Decrypt Duration: {timer.ElapsedMilliseconds}ms";
             }
         }
+        
     }
 
 
